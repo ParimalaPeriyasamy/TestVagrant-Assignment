23b2d048a2e74cc7780bca11711a15cd1ef737d2
ParimalaPeriyasamy <parimalaa2011@gmail.com>
2021-05-09 14:22:51 +0530

    ChromeDriver changes Updated

--- TestVagrant-Assignment-WeatherComparator/src/main/java/com/testvagrant/drivermanager/ChromeDriverManager.java
+++ TestVagrant-Assignment-WeatherComparator/src/main/java/com/testvagrant/drivermanager/ChromeDriverManager.java
@@ -10,11 +10,6 @@ public class ChromeDriverManager implements IDriver {
 
 	private final ThreadLocal<WebDriver> driver = new ThreadLocal<WebDriver>();
 	private final ChromeOptions options = new ChromeOptions();
-	
-	// this class not available outside of this package, use DriverFactory class to create driver object
-	ChromeDriverManager() {
-		super();
-	}
 
 	public void setup() {
 		WebDriverManager.chromedriver().setup();
